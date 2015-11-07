ba6cd944fb1f738f42350f13c2266b1d3e3986a3
tokoyax <getting.foolish@gmail.com>
2015-11-07 02:37:17 +0000

    Fix micropost_spec error

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -24,7 +24,7 @@ describe Micropost do
   end
 
   describe "with content that is too long" do
-    before { @Micropost.content = "a" * 141 }
+    before { @micropost.content = "a" * 141 }
     it { should_not be_valid }
   end
 end
